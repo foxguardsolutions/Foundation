6e33d04abbc0254f7dcb3ce0ae6fcadbd8c76a58
Jordan Kuhn <jtkuhn@foxguardsolutions.com>
2015-07-09 09:03:00 -0400

    🔄 Added validation attributes for ModelState
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/7214649c7b4a6f3e0f2e15b980c6ca234b677f94

---
+++ FGS.Pump.Extensions/ModelStateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FGS.Pump.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        /// <summary>
+        /// Converts the <paramref name="modelState"/> to a dictionary that can be easily serialized.
+        /// </summary>
+        public static IDictionary<string, string[]> ToSerializableDictionary(this ModelStateDictionary modelState)
+        {
+            return modelState.Where(x => x.Value.Errors.Any()).ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray());
+        }
+    }
+}

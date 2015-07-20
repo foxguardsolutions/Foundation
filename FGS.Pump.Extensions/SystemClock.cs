ee4b46acd82269612dfe711188d47a5b601e7e24
Bruce Markham <219281+brucificus@users.noreply.github.com>
2015-07-20 14:38:46 -0400

    🔄 Moved/Renamed PunchClock
    This needed to be in a more centralized location. Made it into an interface resembling the static members on `DateTimeOffset`.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/606d04156ad1ed654850a81945c971ed8b3861d1

---
+++ FGS.Pump.Extensions/SystemClock.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FGS.Pump.Extensions
+{
+    public class SystemClock : IClock
+    {
+        public DateTimeOffset Now 
+        { 
+            get { return DateTimeOffset.Now; } 
+        }
+
+        public DateTimeOffset UtcNow
+        {
+            get { return DateTimeOffset.UtcNow; }
+        }
+    }
+}

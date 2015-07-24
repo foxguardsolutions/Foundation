6607ac1dfab4cdcd844eb119841bf05327f5bd77
FGS-Cthomas <cthomas@foxguardsolutions.com>
2015-07-24 16:52:29 -0400

    🔄 Implement FileOperationsCloudImpl
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/dab36593a49e7e25cd3eb4c7c312c5659e022ed4

--- FGS.Pump.MVC.Support/IFileOperations.cs
+++ FGS.Pump.MVC.Support/IFileOperations.cs
@@ -1,9 +1,19 @@
+using System;
+using System.IO;
 using System.Web;
+using FGS.Pump.Settings.FileOpsSettings;
 
 namespace FGS.Pump.MVC.Support
 {
     public abstract class AFileOperations
     {
+        protected IFileOpsSettings _fileOpsSettings;
+
+        protected AFileOperations(IFileOpsSettings fileOpsSettings)
+        {
+            _fileOpsSettings = fileOpsSettings;
+        }
+
         public abstract string SaveUploadedFile(HttpPostedFileBase uploadedFile);
 
         public  bool FileWasPosted(HttpPostedFileBase file)
@@ -13,8 +23,24 @@ namespace FGS.Pump.MVC.Support
 
         public abstract bool Delete(string path);
 
-        public abstract string GetFullPathToRepoFile(string filename);
+        public string GetFullPathToRepoFile(string filename)
+        {
+            var rawFileName = Path.GetFileName(filename);
+            if (rawFileName != null) return Path.Combine(_fileOpsSettings.FileRepo, rawFileName);
+            throw new InvalidFileNameException(
+                string.Format("The filename \"{0}\" could not be parsed by the filesystem.", filename));
+        }
 
         public abstract bool FileExists(string path);
+
+        public abstract Stream StreamFile(string path);
+    }
+
+    public class InvalidFileNameException : Exception
+    {
+        public InvalidFileNameException(string message) : base(message)
+        {
+            
+        }
     }
 }

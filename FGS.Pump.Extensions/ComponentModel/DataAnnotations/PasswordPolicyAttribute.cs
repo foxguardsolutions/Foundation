55257890b185c5225898a7ca0498c12ccabe3ae7
Jon Savage <jsavage@foxguardsolutions.com>
2015-07-28 11:59:34 -0400

    🔄 Introduced Password Policy.
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/bc8ef4440cb31bb586e1c99b7631a725cd7f2d59

---
+++ FGS.Pump.Extensions/ComponentModel/DataAnnotations/PasswordPolicyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FGS.Pump.Extensions.ComponentModel.DataAnnotations
+{
+    public class PasswordPolicyAttribute : MinLengthAttribute
+    {
+        public PasswordPolicyAttribute(int length = 12) : base(length)
+        {
+            ErrorMessage = string.Format("Password must be at least {0} characters long", length);
+        }
+    }
+}

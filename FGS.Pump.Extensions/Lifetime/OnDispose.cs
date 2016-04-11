551070c1862aca78dc0bc22de251e8246ce85cc6
Bruce Markham <219281+brucificus@users.noreply.github.com>
2016-04-11 17:00:10 -0400

    🔄 Implemented Support for Injected Owned Lifetime
    Now we can inject `Func<… Owned<T>>` and pretend like we are using Autofac.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/c5d8cc7c8cf9e40e30782a9241d27dba55073b11

---
+++ FGS.Pump.Extensions/Lifetime/OnDispose.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FGS.Pump.Extensions.Lifetime
+{
+    /// <summary>
+    /// An <see cref="IDisposable"/> object that exists only to run a custom action when it is disposed.
+    /// </summary>
+    /// <remarks>
+    /// Taken and modified from: https://github.com/foxguardsolutions/psa/blob/38ad1a86f24152d31559249d45a5b5e3b9eb22a0/CPSA.Core/OnDispose.cs
+    /// </remarks>
+    public sealed class OnDispose : Disposable
+    {
+        private readonly Action _action;
+
+        public OnDispose(Action action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+
+            this._action = action;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _action();
+        }
+
+        public static readonly Disposable DoNothing = new OnDispose(() => { });
+    }
+}

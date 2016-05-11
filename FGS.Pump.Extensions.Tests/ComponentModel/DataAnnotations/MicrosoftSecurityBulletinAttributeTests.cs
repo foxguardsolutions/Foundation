02f99b9e89ac09b0a345a7eebfdb5226caf2db50
Austin Howard <ahoward@foxguardsolutions.com>
2016-05-11 16:12:13 -0400

    🔄 Add ❌🤐🔙 Insert API method
    Adds a `❌🤐🔙esController` and a `POST` method for inserting a `❌🤐🔙`
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/63d3a3b1eef47beab5392c910d9bcf01cc2a51bf

---
+++ FGS.Pump.Extensions.Tests/ComponentModel/DataAnnotations/MicrosoftSecurityBulletinAttributeTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using FGS.Pump.Extensions.ComponentModel.DataAnnotations;
+using FGS.Pump.Tests.Support;
+using FGS.Pump.Tests.Support.TestCategories;
+
+using NUnit.Framework;
+
+using Ploeh.AutoFixture;
+
+namespace FGS.Pump.Extensions.Tests.ComponentModel.DataAnnotations
+{
+    [Unit]
+    [TestFixture]
+    public class MicrosoftSecurityBulletinAttributeTests : BaseUnitTest
+    {
+        private MicrosoftSecurityBulletinAttribute _subject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subject = Fixture.Create<MicrosoftSecurityBulletinAttribute>();
+        }
+
+        [TestCaseSource(nameof(FailureCases))]
+        public void IsValid_GivenFailureCases_ReturnsFalse(string bulletinList)
+        {
+            var results = _subject.IsValid(bulletinList);
+
+            Assert.False(results);
+        }
+
+        [TestCaseSource(nameof(SuccessCases))]
+        public void IsValid_GivenSuccessCases_ReturnsTrue(string bulletinList)
+        {
+            var results = _subject.IsValid(bulletinList);
+
+            Assert.True(results);
+        }
+
+        public static IEnumerable<string> FailureCases()
+        {
+            yield return "MS123-45";
+            yield return "MS1-2345";
+            yield return "12-345";
+            yield return "MS-12-345";
+            yield return "MS12345";
+            yield return "MS12-3456";
+            yield return "MS12-345, MS11234";
+            yield return "MS12-345, MS-54-321";
+            yield return "MS12-345,MS11234";
+            yield return "MS13";
+            yield return "SM12-345";
+        }
+
+        public static IEnumerable<string> SuccessCases()
+        {
+            yield return "MS12-345";
+            yield return "MS12-345, MS13-456";
+            yield return "MS12-345,MS13-456";
+            yield return "MS12-345, MS13-456, MS14-567";
+            yield return "MS12-345,MS13-456,MS14-567";
+            yield return "MS13-456, MS14-567, MS12-345";
+        }
+    }
+}
---
+++ FGS.Pump.Extensions/ComponentModel/DataAnnotations/MicrosoftSecurityBulletinAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FGS.Pump.Extensions.ComponentModel.DataAnnotations
+{
+    public class MicrosoftSecurityBulletinAttribute : RegularExpressionAttribute
+    {
+        public MicrosoftSecurityBulletinAttribute()
+            : base(@"MS\d{2}(-)\d{3}(,\s*MS\d{2}(-)\d{3})*$")
+        {
+        }
+    }
+}

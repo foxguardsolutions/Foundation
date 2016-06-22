ba6d9172190617e1e25f4b2ce96696282af98b03
Bruce Markham <219281+brucificus@users.noreply.github.com>
2016-06-22 15:02:23 -0400

    🔄 Split Binding Extensions into Multiple Classes
    CLR references are weird. This simple class/namespace split makes it so that assemblies using some of the `.In(…)` extension methods don't require that we reference all the assemblies containing the types on all of the signatures of the `.In(…)` extension methods. That is, if you are using `.In(…)` for a binding that isn't a web filter, you shouldn't need to reference web assemblies on account of you using `.In(…)`.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/31677fe0b3adbde9ba819bc3338856f899b5e0b8

---
+++ FGS.Pump.Extensions/DI/Mvc/MvcBindingExtensions.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace FGS.Pump.Extensions.DI.Mvc
+{
+    public static class MvcBindingExtensions
+    {
+        /// <remarks>Based on the non-filter version from: https://github.com/ninject/Ninject.Extensions.NamedScope/blob/3957ea5e2e5bf100163ca8e5abd9db6084d33701/src/Ninject.Extensions.NamedScope/NamedScopeExtensionMethods.cs </remarks>
+        public static Ninject.Web.Mvc.FilterBindingSyntax.IFilterBindingNamedWithOrOnSyntax<T> InParentScope<T>(this Ninject.Web.Mvc.FilterBindingSyntax.IFilterBindingInSyntax<T> self)
+        {
+            return self.InScope(
+                context =>
+                    {
+                        const string ScopeParameterName = BindingExtensions.InParentScopeScopeParameterName;
+                        var parentContext = context.Request.ParentContext;
+                        return BindingExtensions.GetOrAddNamedScope(parentContext, ScopeParameterName);
+                    });
+        }
+
+        public static Ninject.Web.Mvc.FilterBindingSyntax.IFilterBindingNamedWithOrOnSyntax<T> In<T>(this Ninject.Web.Mvc.FilterBindingSyntax.IFilterBindingInSyntax<T> self, Scope scope)
+        {
+            switch (scope)
+            {
+                case Scope.Transient:
+                    return self.InTransientScope();
+                case Scope.Parent:
+                    return self.InParentScope();
+                case Scope.PerRequest:
+                    return self.InRequestScope();
+                case Scope.Singleton:
+                    return self.InSingletonScope();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(scope), scope, null);
+            }
+        }
+    }
+}
---
+++ FGS.Pump.Extensions/DI/WebApi/WebApiBindingExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Http.Filters;
+
+namespace FGS.Pump.Extensions.DI.WebApi
+{
+    public static class WebApiBindingExtensions
+    {
+        /// <remarks>Based on the non-filter version from: https://github.com/ninject/Ninject.Extensions.NamedScope/blob/3957ea5e2e5bf100163ca8e5abd9db6084d33701/src/Ninject.Extensions.NamedScope/NamedScopeExtensionMethods.cs </remarks>
+        public static Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingNamedWithOrOnSyntax<T> InParentScope<T>(this Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingInSyntax<T> self)
+            where T : IFilter
+        {
+            return self.InScope(
+                context =>
+                    {
+                        const string ScopeParameterName = BindingExtensions.InParentScopeScopeParameterName;
+                        var parentContext = context.Request.ParentContext;
+                        return BindingExtensions.GetOrAddNamedScope(parentContext, ScopeParameterName);
+                    });
+        }
+
+        public static Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingNamedWithOrOnSyntax<T> In<T>(this Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingInSyntax<T> self, Scope scope)
+            where T : IFilter
+        {
+            switch (scope)
+            {
+                case Scope.Transient:
+                    return self.InTransientScope();
+                case Scope.Parent:
+                    return self.InParentScope();
+                case Scope.PerRequest:
+                    return self.InRequestScope();
+                case Scope.Singleton:
+                    return self.InSingletonScope();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(scope), scope, null);
+            }
+        }
+    }
+}

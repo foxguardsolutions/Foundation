3df613a27ee9c98f97585e2929766da776edd9ce
Bruce Markham <219281+brucificus@users.noreply.github.com>
2016-06-29 11:16:11 -0400

    🔄 Introduced ApiFilterDecoraptor to Control Api Filter Lifetimes
    Ninject was re-using instances of our Api filters regardless as to what lifetime we were binding them with, so this change converts existing Api filter bindings (and makes it easier for future bindings) to bind a decoraptor for each filter when the filter is bound. The decoraptor can then defer lifetime initialization of the actual filter until it is needed. All this prevents us from getting problems from trying to "reuse" `PumpDbContext`, and other fun lifetime problems.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/a21e90bb9dd9bd4ad81feabe6819f321ad5fa0a8

---
+++ FGS.Pump.Extensions/DI/WebApi/ApiActionFilterDecoraptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace FGS.Pump.Extensions.DI.WebApi
+{
+    public class ApiActionFilterDecoraptor<T> : IActionFilter
+        where T : IActionFilter
+    {
+        private readonly Func<T> _filterFactory;
+        private readonly Lazy<bool> _lazyAllowMultiple;
+
+        public ApiActionFilterDecoraptor(Func<T> filterFactory)
+        {
+            _filterFactory = filterFactory;
+            _lazyAllowMultiple = new Lazy<bool>(filterFactory().AllowMultiple);
+        }
+
+        public bool AllowMultiple => _lazyAllowMultiple.Value;
+
+        public Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            return _filterFactory().ExecuteActionFilterAsync(actionContext, cancellationToken, continuation);
+        }
+    }
+}
--- FGS.Pump.Extensions/DI/WebApi/WebApiBindingExtensions.cs
+++ FGS.Pump.Extensions/DI/WebApi/WebApiBindingExtensions.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Web.Http.Filters;
+
+using Ninject.Syntax;
+using Ninject.Web.WebApi.FilterBindingSyntax;
 
 namespace FGS.Pump.Extensions.DI.WebApi
 {
     public static class WebApiBindingExtensions
     {
         /// <remarks>Based on the non-filter version from: https://github.com/ninject/Ninject.Extensions.NamedScope/blob/3957ea5e2e5bf100163ca8e5abd9db6084d33701/src/Ninject.Extensions.NamedScope/NamedScopeExtensionMethods.cs </remarks>
-        public static Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingNamedWithOrOnSyntax<T> InParentScope<T>(this Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingInSyntax<T> self)
+        public static IFilterBindingNamedWithOrOnSyntax<T> InParentScope<T>(this IFilterBindingInSyntax<T> self)
             where T : IFilter
         {
             return self.InScope(
@@ -18,7 +22,7 @@ namespace FGS.Pump.Extensions.DI.WebApi
                     });
         }
 
-        public static Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingNamedWithOrOnSyntax<T> In<T>(this Ninject.Web.WebApi.FilterBindingSyntax.IFilterBindingInSyntax<T> self, Scope scope)
+        public static IFilterBindingNamedWithOrOnSyntax<T> In<T>(this IFilterBindingInSyntax<T> self, Scope scope)
             where T : IFilter
         {
             switch (scope)
@@ -33,6 +37,56 @@ namespace FGS.Pump.Extensions.DI.WebApi
                     return self.InSingletonScope();
                 default:
                     throw new ArgumentOutOfRangeException(nameof(scope), scope, null);
+            }
+        }
+
+        public static void BindDecoraptedHttpAuthorizationFilter<T>(this IBindingRoot kernel, FilterScope filterScope, Scope httpScope, Func<IFilterBindingWhenSyntax<IAuthorizationFilter>, IFilterBindingInNamedWithOrOnSyntax<IAuthorizationFilter>> when = null, Func<IFilterBindingWithSyntax<IAuthorizationFilter>, IFilterBindingWithOrOnSyntax<IAuthorizationFilter>> with = null)
+            where T : IAuthorizationFilter
+        {
+            var decoraptorBound = BindingRootExtensions.BindHttpFilter<ApiAuthorizationFilterDecoraptor<T>>(kernel, filterScope);
+
+            var name = "BindHttpFilter" + Guid.NewGuid();
+
+            if (when != null)
+            {
+                when(decoraptorBound).In(httpScope).Named(name);
+            }
+            else
+            {
+                decoraptorBound.In(httpScope).Named(name);
+            }
+
+            var actualBound = kernel.Bind<T>().ToSelf().WhenAnyAncestorNamed(name).In(httpScope);
+
+            if (with != null)
+            {
+                var syntaxAdapter = new DecoraptedApiFilterBindingWithSyntaxAdapter<T, IAuthorizationFilter>(actualBound);
+                with(syntaxAdapter);
+            }
+        }
+
+        public static void BindDecoraptedHttpActionFilter<T>(this IBindingRoot kernel, FilterScope filterScope, Scope httpScope, Func<IFilterBindingWhenSyntax<IActionFilter>, IFilterBindingInNamedWithOrOnSyntax<IActionFilter>> when = null, Func<IFilterBindingWithSyntax<IActionFilter>, IFilterBindingWithOrOnSyntax<IActionFilter>> with = null)
+            where T : IActionFilter
+        {
+            var decoraptorBound = BindingRootExtensions.BindHttpFilter<ApiActionFilterDecoraptor<T>>(kernel, filterScope);
+
+            var name = "BindHttpFilter" + Guid.NewGuid();
+
+            if (when != null)
+            {
+                when(decoraptorBound).In(httpScope).Named(name);
+            }
+            else
+            {
+                decoraptorBound.In(httpScope).Named(name);
+            }
+
+            var actualBound = kernel.Bind<T>().ToSelf().WhenAnyAncestorNamed(name).In(httpScope);
+
+            if (with != null)
+            {
+                var syntaxAdapter = new DecoraptedApiFilterBindingWithSyntaxAdapter<T, IActionFilter>(actualBound);
+                with(syntaxAdapter);
             }
         }
     }

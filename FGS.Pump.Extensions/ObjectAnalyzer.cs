e6f33b2d647e6ce16d93ac162d8588e38e332f70
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-08-08 16:25:32 -0400

    🔄 Creates ❌🤐🔙 from ❌🤐🔙Schema❌🤐🔙4Json
    ❌🤐🔙 schema ❌🤐🔙s are expected to be consistent, thus a dynamic object
    deserialized from the ❌🤐🔙 Schema ❌🤐🔙 4 json feed will be passed to
    appropriate objects to traverse the json. These objects will traverse
    the dynamic JSON object as expected based off of the schema ❌🤐🔙.
    
    As of this commit there is only support for schema ❌🤐🔙 4 JSON.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/fcecd6dd9678119619f68da588ea43a8dcbb4964

---
+++ FGS.Pump.Extensions/ObjectAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
+
+namespace FGS.Pump.Extensions
+{
+    public static class ObjectAnalyzer
+    {
+        public static bool HasProperty(object obj, string propertyName)
+        {
+            var type = obj.GetType();
+            if (type == typeof(JObject))
+                return ((JObject)obj)[propertyName] != null;
+            if (type == typeof(ExpandoObject))
+                return ((IDictionary<string, object>)obj).ContainsKey(propertyName);
+            return type.GetProperties().Any(p => p.Name == propertyName);
+        }
+    }
+}

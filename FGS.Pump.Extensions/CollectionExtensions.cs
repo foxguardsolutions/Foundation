a78a9e95e54af28f6c3981c06ecbdce7b73baf32
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-08-09 14:11:45 -0400

    🔄 Adds Linq extension for TakeLast
    A CollectionExtensions class has been created to write custom Linq
    functions. As of this commit, a function for TakeLast has been written
    as well as Tests to verify the functionality.
    
    This extension was added to help parse the two-part file extension of
    the Uri for the ❌🤐🔙 feeds.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/9059b48e36de34a8a5f98d5497c9946593734932

---
+++ FGS.Pump.Extensions/CollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FGS.Pump.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static ICollection<T> TakeLast<T>(this ICollection<T> collection, int count)
+        {
+            return collection.Reverse().Take(count).Reverse().ToList();
+        }
+    }
+}

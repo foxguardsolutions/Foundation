437eb0fbc384aa335851f88015690492c2bebf37
Bruce Markham <219281+brucificus@users.noreply.github.com>
2018-01-23 13:32:46 -0500

    🔄 Defines General-Purpose Pagination Primitives
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/ecbf6e65d0288b1e4294f0b96d9e4482577789ec

---
+++ FGS.Pump.Extensions/PaginationSpecification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FGS.Pump.Extensions
+{
+    public struct PaginationSpecification : IEquatable<PaginationSpecification>
+    {
+        public int PageNumber { get; }
+
+        public int PageSize { get; }
+
+        public PaginationSpecification(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+
+        public static PaginationSpecification FirstPageOfSize(int pageSize) => new PaginationSpecification(1, pageSize);
+
+        public PaginationSpecification Next() => new PaginationSpecification(PageNumber + 1, PageSize);
+
+        public PaginationSpecification? Previous()
+        {
+            return PageNumber <= 0
+                ? (PaginationSpecification?)null
+                : new PaginationSpecification(PageNumber - 1, PageSize);
+        }
+
+        public bool Equals(PaginationSpecification other) => PageNumber == other.PageNumber && PageSize == other.PageSize;
+
+        public override bool Equals(object obj)
+        {
+            return obj is null
+                ? false
+                : obj is PaginationSpecification specification && Equals(specification);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (PageNumber * 397) ^ PageSize;
+            }
+        }
+    }
+}

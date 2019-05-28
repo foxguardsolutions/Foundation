e8dde9bb6950cc3879b1720d389601c9d62f0232
Bruce Markham <219281+brucificus@users.noreply.github.com>
2019-05-28 14:06:58 -0400

    🔄 Translates Between ASP.NET API Differences for Routing
    Removes vestiges of the old way routing was wired. Fixes MultipleButtonAttribute. Fixes AutoMapper profiles that rely on routes to use the recently-added `SingletonCompatibleUrlHelper`.
    
    WARNING: This commit is part of a protracted series of commits wherein the code is being converted to ASP.NET Core.
             Compilation and correct execution of both applications and any/all tests is likely broken.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/3686d61814170f048d2865a9d9789e8d0db71f68

--- Extensions/FGS.Pump.MVC.Support/Startup/IRouteRegistrar.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Routing;
-
-namespace FGS.Pump.MVC.Support.Startup
-{
-    public interface IRouteRegistrar
-    {
-        void RegisterRoutes(IRouteBuilder routes);
-    }
-}

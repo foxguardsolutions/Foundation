c2138bd3c2740048fa639121ccd5654efddb4b65
Bruce Markham <219281+brucificus@users.noreply.github.com>
2019-06-06 10:32:16 -0400

    🔄 Adds Support for Idiomatically Configuring Forwarded Headers on ASP.NET Core 2.2 w/ AWS ALBs
    ASP.NET Core has support for using forwarded headers from reverse-proxies, however it can be tricky to configure correctly. This adds an adapter than configures the underlying support for forwarded headers, while expressing its API in terms of the configuration of an expected AWS Application Load Balancer as the reverse-proxy.
    
    NOTE: This adds new code that is not yet used or automatically tested. This new code will be used as of the coming ASP.NET Core upgrade, and has been manually tested to be working "well enough" for now.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/ac02c18cddf40669b4ef88a8a519d6c7711a9601

---
+++ Extensions/FGS.AspNetCore.Http.Extensions.AWS.ALB/AwsAlbApplicationBuilderExtensions.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace FGS.AspNetCore.Http.Extensions.AWS.ALB
+{
+    public static class AwsAlbApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Forwards proxied headers on to current request, for cases when proxy is expected to be an AWS Application Load Balancer.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="tlsOffloaded">Indicates whether or not the AWS ALB is configured to offload TLS, and therefor whether or not to expect the <c>X-Forwarded-Proto</c> header. The default is <c>true</c>.</param>
+        /// <param name="hostIsRewritten">Indicates whether or not the AWS ALB is configured to rewrite the <c>Host</c> header, and therefor whether or not to expect the <c>X-Forwarded-Host</c> header. The default is <c>false</c>.</param>
+        /// <remarks>
+        /// <para>
+        ///     This adapter on top of <see cref="Microsoft.AspNetCore.Builder.ForwardedHeadersExtensions.UseForwardedHeaders"/> is needed when the application is running behind an AWS Application Load Balancer and the consumer wishes to leverage forwarded headers.
+        ///     It is notable that the underlying middleware does not activate unless it is configured in a way that precisely matches incoming requests, which is tricky because AWS ALBs only forward headers based on how they themselves are configured.
+        ///     This helper bridges the gap between the two by allowing a consumer to express expected headers in terms relating to how the AWS ALB is configured.
+        /// </para>
+        /// <para>
+        ///     ⚠WARNING: The implementation of this explicitly configures the underlying middleware to trust the forwarding headers regardless as to network source of the connection. This could pose a spoofing risk on untrusted networks.
+        /// </para>
+        /// </remarks>
+        public static IApplicationBuilder UseForwardedHeadersFromAwsApplicationLoadBalancer(this IApplicationBuilder app, bool tlsOffloaded = true, bool hostIsRewritten = false)
+        {
+            var forwardedHeaders = ForwardedHeaders.XForwardedFor;
+
+            if (tlsOffloaded)
+            {
+                forwardedHeaders |= ForwardedHeaders.XForwardedProto;
+            }
+
+            if (hostIsRewritten)
+            {
+                forwardedHeaders |= ForwardedHeaders.XForwardedHost;
+            }
+
+            var options = new ForwardedHeadersOptions()
+            {
+                ForwardedHeaders = forwardedHeaders,
+            };
+
+            // We have to explicitly clear these. They need to be empty in order for `ForwardedHeadersMiddleware` to skip IP validation.
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+
+            return app.UseForwardedHeaders(options);
+        }
+    }
+}

022e0961145a3bc8502320a7681f2f4eb4fd9da0
Bruce Markham <219281+brucificus@users.noreply.github.com>
2019-08-22 15:40:43 -0400

    🔄 Adds Generic ASP.NET Health Check for Entity Framework Readiness
    This adds a health check implementation that can be used to check if an Entity Framework DbContext is ready for use. Inspired by [`AspNetCore.HealthChecks.SqlServer`](https://github.com/xabaril/AspNetCore.Diagnostics.HealthChecks).
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Pump/commit/621f161b01a49b56f7cb0d121cebc05b2f68718a

---
+++ Extensions/FGS.Extensions.Diagnostics.HealthChecks.EntityFramework/EntityFrameworkHealthCheck.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace FGS.Extensions.Diagnostics.HealthChecks.EntityFramework
+{
+    public class EntityFrameworkHealthCheck<TDbContext> : IHealthCheck
+        where TDbContext : DbContext
+    {
+        private readonly TDbContext _dbContext;
+        private readonly Func<TDbContext, IQueryable<bool>> _healthQuery;
+
+        public EntityFrameworkHealthCheck(TDbContext dbContext, Func<TDbContext, IQueryable<bool>> healthQuery)
+        {
+            _dbContext = dbContext;
+            _healthQuery = healthQuery;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "For health checks, upstream exception types are unknowable by definition")]
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                await _healthQuery(_dbContext).FirstAsync(cancellationToken).ConfigureAwait(false);
+
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, null, ex);
+            }
+        }
+    }
+}

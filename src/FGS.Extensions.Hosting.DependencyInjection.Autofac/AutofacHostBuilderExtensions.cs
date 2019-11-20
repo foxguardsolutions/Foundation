a3e07220ff5d613455cccf0a9a3262f57fc02840
Bruce Markham <219281+brucificus@users.noreply.github.com>
2019-11-20 13:21:44 -0500

    ♻🐛 Fixes Broken Package Separation for `FGS.*.DependencyInjection.Autofac`
    A previous refactoring attempted to split this library in two by varying the assembly name based on the target framework. Unfortunately, this did not have the intended effect of varying the output package name (which is documented as defaulting to the assembly name.) Additionally, this also had the misfortune of making the NuGet Package Manager tool in Visual Studio become unstable and crash when attempting to manage dependencies elsewhere in the parent SLN.
    
    This fixes the attempted split by duplicating the project and then specializing each copy for the target frameworks it is intended for.

--- src/FGS.Extensions.Hosting.DependencyInjection.Autofac/AutofacHostBuilderExtensions.cs
+++ src/FGS.Extensions.Hosting.DependencyInjection.Autofac/AutofacHostBuilderExtensions.cs
@@ -9,24 +9,24 @@ using FGS.Autofac.DynamicScoping.Abstractions.Specialized;
 using FGS.Extensions.DependencyInjection.Autofac;
 
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-namespace FGS.AspNetCore.Hosting.Extensions.DependencyInjection.Autofac
+namespace FGS.Extensions.Hosting.DependencyInjection.Autofac
 {
     /// <summary>
-    /// Extension methods for the <see cref="IWebHostBuilder"/> interface.
+    /// Extension methods for the <see cref="IHostBuilder"/> interface.
     /// </summary>
     /// <remarks>Taken and modified from: https://github.com/autofac/Autofac.AspNetCore/blob/e8254995519b96c568de194371edad4d2c20db0e/src/Autofac.Integration.AspNetCore/AutofacWebHostBuilderExtensions.cs. </remarks>
-    public static class AutofacWebHostBuilderExtensions
+    public static class AutofacHostBuilderExtensions
     {
         /// <summary>
         /// Adds a custom implementation of Autofac's <see cref="IServiceProviderFactory{TContainerBuilder}"/> to the <see cref="IServiceCollection"/>
         /// being built by <paramref name="builder"/>.
         /// </summary>
-        /// <param name="builder">The <see cref="IWebHostBuilder"/> instance being configured.</param>
+        /// <param name="builder">The <see cref="IHostBuilder"/> instance being configured.</param>
         /// <param name="forEachModule">An optional action used to configure each <see cref="IModule"/> being added to the container.</param>
         /// <param name="configurationAction">An optional action used to configure the container.</param>
-        /// <returns>The existing <see cref="IWebHostBuilder"/> instance.</returns>
+        /// <returns>The existing <see cref="IHostBuilder"/> instance.</returns>
         /// <typeparam name="TModulesProvider">The type of provider that can enumerate all of the Autofac modules to be registered.</typeparam>
         /// <example>
         /// <code>
@@ -36,7 +36,7 @@ namespace FGS.AspNetCore.Hosting.Extensions.DependencyInjection.Autofac
         ///         .UseStartup&lt;Startup&gt;();
         /// </code>
         /// </example>
-        public static IWebHostBuilder UseAutofacWithModulesProvider<TModulesProvider>(this IWebHostBuilder builder, Action<IModule> forEachModule = null, Action<ContainerBuilder> configurationAction = null)
+        public static IHostBuilder UseAutofacWithModulesProvider<TModulesProvider>(this IHostBuilder builder, Action<IModule> forEachModule = null, Action<ContainerBuilder> configurationAction = null)
             where TModulesProvider : IModulesProvider, new()
         {
             void ForEachModule(IModule module)
@@ -45,7 +45,7 @@ namespace FGS.AspNetCore.Hosting.Extensions.DependencyInjection.Autofac
                 forEachModule?.Invoke(module);
             }
 
-            return builder.ConfigureServices(services => services.AddAutofacWithModulesProvider<TModulesProvider>(ForEachModule, configurationAction));
+            return builder.UseServiceProviderFactory(new ModulesProviderBasedAutofacServiceProviderFactory<TModulesProvider>(ForEachModule, configurationAction));
         }
     }
 }
